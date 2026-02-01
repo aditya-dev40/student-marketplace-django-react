2fb8b15ce58bccb7764126aae01a6646b62fe3be
Addy4080 <adityathorat80.in@gmail.com>
2026-02-01 22:46:07 +0530

    Add store frontend layout and category/product models
    - Added base layout and store homepage UI
    - Implemented navbar and hero section for StudentSwap
    - Created Category and Product models
    - Registered models in Django admin with slug support
    - Enabled saving categories and products in the database

---
+++ ecommerce/store/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model): # it is inheritance: it is inheriting form models.Model to access its
+                            # properties
+
+    name = models.CharField(max_length=255, db_index=True)
+
+    # Slug is used for unique, readable URLs (e.g., categories/books/)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    
+    title = models.CharField(max_length=250)
+
+    brand = models.CharField(max_length=250, default='un-branded')
+
+    description = models.TextField(blank=True)
+
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    slug = models.SlugField(max_length=250)
+
+    image = models.ImageField(upload_to='image/')
+
+    class Meta:
+
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.title
